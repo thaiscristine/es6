1b918e8dc5641dfc89773878e782596cbfbab75e
Thais Cristine <this.cristine06@gmail.com>
2020-04-07 13:23:18 -0300

    funções de arrays

--- bundle.js
+++ bundle.js
@@ -1,94 +1,23 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // map percorre todos os itens do array e tem acesso aos mesmos e ao index
 
-function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+var newArr = arr.map(function (item, index) {
+  return item + index;
+}); //console.log(newArr);
+// reduce percorre todo o array trabalhando com o valor total(that starts 0) e o valor do próximo item
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+var sum = arr.reduce(function (total, next) {
+  return total + next; // 0 + 1 // 1 + 2 // 3 + 3 // 6 + 4 // ... 
+}); //console.log(sum);
+// filter percorre todo o array com acesso a cada item. Retorna sempre true or false
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+var filter = arr.filter(function (item) {
+  return item % 2 === 0; // operacao de módulo
+}); //console.log(filter);
+// find percorre o array verificando item por item. Retorna o valor se encontrado e undefined caso o contrário
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var List = /*#__PURE__*/function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
-  }
-
-  _createClass(List, [{
-    key: "addTodo",
-    value: function addTodo(data) {
-      this.data.push(data);
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}(); // Efeito de herança
-
-
-var TodoList = /*#__PURE__*/function (_List) {
-  _inherits(TodoList, _List);
-
-  var _super = _createSuper(TodoList);
-
-  function TodoList() {
-    var _this;
-
-    _classCallCheck(this, TodoList);
-
-    _this = _super.call(this);
-    _this.user = "Thais";
-    return _this;
-  }
-
-  _createClass(TodoList, [{
-    key: "mostrarUsuario",
-    value: function mostrarUsuario() {
-      console.log(this.user);
-    }
-  }]);
-
-  return TodoList;
-}(List); // classes estáticas nao precisam de constructor
-
-
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}(); //instanciando a classe -> variável que armazena a TodoList();
-
-
-var MinhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function () {
-  MinhaLista.addTodo('Novo Todo x');
-};
-
-MinhaLista.mostrarUsuario(); // Chama a classe sem instanciar
-
-console.log(Matematica.soma(2, 3));
+var find = arr.find(function (item) {
+  return item === 2;
+});
+console.log(find);
