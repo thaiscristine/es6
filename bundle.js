fd022450da320aeac78116d030ddf43662084586
Thais Cristine <this.cristine06@gmail.com>
2020-04-08 10:37:49 -0300

    Valores padrões

--- bundle.js
+++ bundle.js
@@ -1,38 +1,20 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 6]; // arrow function
+// Valores padrões
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+}
 
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr);
+console.log(soma(1)); // 7
 
-var teste = function teste() {
-  return 'teste';
+console.log(soma()); // 9
+
+var soma2 = function soma2() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-console.log(teste());
-
-var teste1 = function teste1() {
-  return 'teste';
-};
-
-var teste2 = function teste2() {
-  return [1, 2, 3];
-};
-
-var teste3 = function teste3() {
-  return 3;
-};
-
-console.log(teste1());
-console.log(teste2());
-console.log(teste3()); //object needes a parenteses 
-
-var teste4 = function teste4() {
-  return {
-    nome: 'Thais'
-  };
-};
-
-console.log(teste4());
+console.log(soma2());
