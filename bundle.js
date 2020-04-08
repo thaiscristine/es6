e4f77d9bf8949f806060b772888806a8bef00efc
Thais Cristine <this.cristine06@gmail.com>
2020-04-08 10:46:12 -0300

    Desestruturaação

--- bundle.js
+++ bundle.js
@@ -1,20 +1,19 @@
 "use strict";
 
-// Valores padrões
-function soma() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
-  return a + b;
+var usuario = {
+  nome: 'Thais',
+  idade: 25,
+  endereco: {
+    cidade: 'SBC',
+    estado: 'SP'
+  }
+}; //const { nome, idade, endereco: { estado } } = usuario;
+//console.log(estado);
+
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
 }
 
-console.log(soma(1)); // 7
-
-console.log(soma()); // 9
-
-var soma2 = function soma2() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
-  return a + b;
-};
-
-console.log(soma2());
+mostraNome(usuario);
