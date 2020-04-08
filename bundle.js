259c382f9bf8dc95ce88c7c490abb9e728642b5c
Thais Cristine <this.cristine06@gmail.com>
2020-04-08 10:31:26 -0300

    arrow functions

--- bundle.js
+++ bundle.js
@@ -1,23 +1,38 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // map percorre todos os itens do array e tem acesso aos mesmos e ao index
+var arr = [1, 3, 4, 5, 6]; // arrow function
 
-var newArr = arr.map(function (item, index) {
-  return item + index;
-}); //console.log(newArr);
-// reduce percorre todo o array trabalhando com o valor total(that starts 0) e o valor do próximo item
-
-var sum = arr.reduce(function (total, next) {
-  return total + next; // 0 + 1 // 1 + 2 // 3 + 3 // 6 + 4 // ... 
-}); //console.log(sum);
-// filter percorre todo o array com acesso a cada item. Retorna sempre true or false
-
-var filter = arr.filter(function (item) {
-  return item % 2 === 0; // operacao de módulo
-}); //console.log(filter);
-// find percorre o array verificando item por item. Retorna o valor se encontrado e undefined caso o contrário
-
-var find = arr.find(function (item) {
-  return item === 2;
+var newArr = arr.map(function (item) {
+  return item * 2;
 });
-console.log(find);
+console.log(newArr);
+
+var teste = function teste() {
+  return 'teste';
+};
+
+console.log(teste());
+
+var teste1 = function teste1() {
+  return 'teste';
+};
+
+var teste2 = function teste2() {
+  return [1, 2, 3];
+};
+
+var teste3 = function teste3() {
+  return 3;
+};
+
+console.log(teste1());
+console.log(teste2());
+console.log(teste3()); //object needes a parenteses 
+
+var teste4 = function teste4() {
+  return {
+    nome: 'Thais'
+  };
+};
+
+console.log(teste4());
